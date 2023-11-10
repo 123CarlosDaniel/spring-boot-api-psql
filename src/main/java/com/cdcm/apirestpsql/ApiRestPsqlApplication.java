352f876b02362a1dd4477fb3b5005d097f27fca8
carlos_daniel <carloschoqque123@gmail.com>
2023-11-10 15:41:39 -0500

    initial commit

---
+++ src/main/java/com/cdcm/apirestpsql/ApiRestPsqlApplication.java
@@ -0,0 +1,13 @@
+package com.cdcm.apirestpsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestPsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestPsqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdcm/apirestpsql/ApiRestPsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdcm.apirestpsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestPsqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
