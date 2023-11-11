498f829c1ea36c618335beae5cd9573c5f7b688e
carlos_daniel <carloschoqque123@gmail.com>
2023-11-10 23:55:15 -0500

    update Person service

--- src/main/java/com/cdcm/apirestpsql/service/imp/PersonServiceImp.java
+++ src/main/java/com/cdcm/apirestpsql/service/imp/PersonServiceImp.java
@@ -1,8 +1,32 @@
 package com.cdcm.apirestpsql.service.imp;
 
+import com.cdcm.apirestpsql.dto.PersonDto;
+import com.cdcm.apirestpsql.entity.Person;
+import com.cdcm.apirestpsql.entity.Product;
+import com.cdcm.apirestpsql.repository.PersonRepository;
 import com.cdcm.apirestpsql.service.interfaces.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
 
 @Service
 public class PersonServiceImp implements PersonService {
+
+    @Autowired
+    private PersonRepository personRepository;
+    @Override
+    public Person createPerson(PersonDto personDto) {
+        Person person = Person.builder()
+                .name(personDto.getName())
+                .email(personDto.getEmail())
+                .password(personDto.getPassword())
+                .products(personDto.getProducts().stream()
+                        .map(prod -> Product.builder()
+                                .price(prod.getPrice())
+                                .name(prod.getName()).build())
+                        .collect(Collectors.toList()))
+                .build();
+        return personRepository.save(person);
+    }
 }
--- src/main/java/com/cdcm/apirestpsql/service/interfaces/PersonService.java
+++ src/main/java/com/cdcm/apirestpsql/service/interfaces/PersonService.java
@@ -1,4 +1,8 @@
 package com.cdcm.apirestpsql.service.interfaces;
 
+import com.cdcm.apirestpsql.dto.PersonDto;
+import com.cdcm.apirestpsql.entity.Person;
+
 public interface PersonService {
+    Person createPerson(PersonDto personDto);
 }
