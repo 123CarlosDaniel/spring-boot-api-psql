012597dd3cdbab42ba93ad7edaac58d9f5aed58d
carlos_daniel <carloschoqque123@gmail.com>
2023-11-11 00:07:18 -0500

    update Person repository test to use embedded database

--- src/test/java/com/cdcm/apirestpsql/repository/PersonRepositoryTest.java
+++ src/test/java/com/cdcm/apirestpsql/repository/PersonRepositoryTest.java
@@ -2,29 +2,27 @@ package com.cdcm.apirestpsql.repository;
 
 import com.cdcm.apirestpsql.entity.Person;
 import com.cdcm.apirestpsql.entity.Product;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
+@DataJpaTest
 class PersonRepositoryTest {
 
     @Autowired
     private PersonRepository personRepository;
 
-    @Test
-    public void getPersons() {
-        List<Person> personList = personRepository.findAll();
-        System.out.println("personList = " + personList);
-    }
+    @Autowired
+    private TestEntityManager testEntityManager;
 
-    @Test
-    public void savePerson() {
+    @BeforeEach
+    public void setUp() {
         Product product1 = Product.builder()
                 .name("oil")
                 .price(BigDecimal.valueOf(10))
@@ -39,8 +37,19 @@ class PersonRepositoryTest {
                 .password("123456")
                 .products(List.of(product1, product2))
                 .build();
-        personRepository.save(person);
+        testEntityManager.persist(person);
+    }
 
+    @Test
+    public void getPersons() {
+        List<Person> personList = personRepository.findAll();
+        System.out.println("personList = " + personList);
+    }
+
+    @Test
+    public void savePerson() {
+        Person person = personRepository.findPersonByEmail("pepe@gmail.com").get();
+        assertEquals(person.getName(), "Pepe");
     }
 }
 
